5c1ad02728a2ad1c22eceb9a3baebb6c9be4565a
vtu26282-netizen <vtu26282@veltech.edu.in>
2026-02-11 11:13:37 +0530

    Time: 18 ms (75.48%), Space: 107.3 MB (53.45%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,16 +1,13 @@
-#include <vector>
-#include <stack>
-using namespace std;
+
 
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
         vector<int> answer(n, 0);
-        stack<int> st;  // stores indices
+        stack<int> st;  
         
         for (int i = 0; i < n; i++) {
-            // If current temperature is greater than stack top
             while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
                 int prevIndex = st.top();
                 st.pop();
