286f518e3883bfae4b18017738113dab2b260755
vtu26282-netizen <vtu26282@veltech.edu.in>
2026-02-11 09:28:08 +0530

    Time: 1 ms (18.88%), Space: 23.9 MB (54.85%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int pos = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) nums[pos++] = nums[i];
+        }
+        while (pos < nums.size()) nums[pos++] = 0;
+    }
+};
